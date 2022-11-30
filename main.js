208fb43e4c64fd4e07c7e35a81087fe0f58b82b3
Airidas <airidassien64@gmail.com>
2022-11-30 18:47:16 +0000

    created functions which allows x and o to be displayed in the grid only works if start button is clicked

--- main.js
+++ main.js
@@ -22,3 +22,22 @@ const winConditions = [
   ["0", "4", "8"],
   ["2", "4", "6"],
 ];
+
+// startPlaying function which allows "X" or "O" to be displayed using a click function (starts the game when the button is clicked)(allowing each cell/box to be clicked)
+const startPlaying = () => {
+  boxes.forEach((cells) => {
+    cells.addEventListener("click", cellClicked);
+    music.volume = 0.1;
+    music.play();
+  });
+};
+
+// Allows to switch from "X" to "O"
+// ? if its true to will do the next one
+function playerChange() {
+  statusText.innerHTML = `Player ${currentPlayer}'s turn`;
+  // If the current player is = "X" we will reassign current player with "O" other wise "X" (this allows players to take turns e,g starts with X then O)
+  currentPlayer = currentPlayer === "X" ? "O" : "X";
+
+  // console.log(currentPlayer);
+}
